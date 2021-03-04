4bd2a02b314e5606d145f8af459d28461bd04c3c
shermosa <shermosa@hotels.com>
2021-03-04 17:10:09 +0000

    resproducing the error

--- src/test/resources/serdeTest/hql_custom_serde.sql
+++ src/test/resources/serdeTest/hql_custom_serde.sql
@@ -2,7 +2,9 @@ CREATE EXTERNAL TABLE customSerdeTable (s1 string, s2 string, s3 string)
     ROW FORMAT SERDE 'com.klarna.hiverunner.ToUpperCaseSerDe'
         WITH SERDEPROPERTIES (
             "key"="value",
-            "KEY"= "VALUE"
+            "KEY"= "VALUE",
+            "key1" = "\\"
+
         )
         STORED AS TEXTFILE
 LOCATION '${hiveconf:hadoop.tmp.dir}/customSerde';
